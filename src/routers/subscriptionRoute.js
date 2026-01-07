128a2ef89fe80553e5df6e8d502f9def4c8bf598
Walid <walid.ledo78@gmail.com>
2026-01-07 18:40:59 +0200

    Update: IAP V1

--- src/routers/subscriptionRoute.js
+++ src/routers/subscriptionRoute.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const subController = require('../services/SupscriptionController');
 const verifyToken = require('../middlewares/verifyToken');
 
-router.post('/subscribe', verifyToken, subController.updateSubscription);
+//router.post('/subscribe', verifyToken, subController.updateSubscription);
 
 router.post('/verify-purchase', verifyToken, subController.verifyPurchase);
-router.get('/current', verifyToken, subController.getCurrentSubscription);
+/*router.get('/current', verifyToken, subController.getCurrentSubscription);
 
 // إلغاء الاشتراك
 router.post('/cancel', verifyToken, subController.cancelSubscription);
@@ -16,6 +16,6 @@ router.post('/cancel', verifyToken, subController.cancelSubscription);
 router.post('/renew', verifyToken, subController.renewSubscription);
 
 // التحقق من حالة الاشتراك
-router.get('/status', verifyToken, subController.getCurrentSubscription);
+router.get('/status', verifyToken, subController.getCurrentSubscription);*/
 
 module.exports = router;
